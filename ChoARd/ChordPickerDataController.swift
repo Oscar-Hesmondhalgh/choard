835e20a50f6519d2579ab032b104706fb77b2b85
Oscar Hesmondhalgh <oscar.hesmondhalgh@focusrite.com>
2022-09-13 16:15:19 +0100

    Merge chord data logic

--- ChoARd/ChordPickerDataController.swift
+++ ChoARd/ChordPickerDataController.swift
@@ -3,48 +3,137 @@ import UIKit
 
 class ChordDataSource: NSObject, UIPickerViewDelegate, UIPickerViewDataSource {
     private let rootValues: [String] = [
-        "C",
-        "D",
-        "E",
-        "F",
-        "G",
-        "A",
-        "B",
+        "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"
     ]
 
     private let typeValues: [String] = [
-        "Major",
-        "Minor"
+        "major",
+        "minor"
     ]
 
-    private let jazzValues: [String] = [
-        "Cool",
-        "Wanky",
-        "Unlistenable"
-    ]
+    let scales = ["major": [0,2,4,5,7,9,11], "minor": [0,2,3,5,7,8,10]];
 
-    private let dataSources: [[String]]
+    func getChromaticScale(root: String) -> [String] {
+        let idx = rootValues.firstIndex(of: root)
+        return Array(rootValues[idx!...] + rootValues[..<idx!])
+    }
 
-    override init() {
-        dataSources = [
-            rootValues,
-            typeValues,
-            jazzValues
+    func getScale(chromaticScale: [String], quality: String) -> [String] {
+        var scale: Array<String> = []
+
+        for value in scales[quality]!{
+            scale.append(String(chromaticScale[value]))
+        }
+
+        return scale
+    }
+
+    func getTriad(scale: [String]) -> [String]{
+        return [scale[0], scale[2], scale[4]]
+    }
+
+    func getSeventh(scale: [String]) -> [String]{
+        return [scale[6]]
+    }
+
+    func getSuspendedVoicing(scale: [String]) -> [String]{
+        return [
+            scale[0],
+            scale[2],
+            scale[4],
+            scale[5],
+            scale[6],
+            scale[1],
+            scale[2],
+            scale[4]
         ]
+    }
 
-        super.init()
+
+    func addOctaveNotation(chord: [String]) -> [String] {
+        var highestNote: String = ""
+        var highestOctave: Int = 0
+        var updatedChord: [String] = []
+        let chromaticScale: [String] = getChromaticScale(root: chord.first!)
+
+        for note in chord {
+            if (updatedChord.count > 0){
+                let currentHighestNoteIndex = chromaticScale.firstIndex(of: highestNote)
+                let nextNoteIndex = chromaticScale.firstIndex(of: note)
+
+                if (nextNoteIndex! < currentHighestNoteIndex!){
+                    highestOctave += 1
+                }
+
+                updatedChord.append(note + String(highestOctave))
+            } else {
+                updatedChord.append(note + "0")
+            }
+
+            highestNote = note
+        }
+
+        return updatedChord
+    }
+
+    func summonJacob(scale: [String]) -> [String]{
+        var unused = Array(scale[1...])
+        unused.shuffle()
+
+        return [scale[0]] + unused
+    }
+
+    func getNotes(root: String, quality: String, jazziness: Jazziness) -> [String]{
+        var chord: [String] = []
+        let chromatic = getChromaticScale(root: root)
+        let scale = getScale(chromaticScale: chromatic, quality: quality)
+
+        if (jazziness == Jazziness.NONE){
+            chord = getTriad(scale: scale)
+        }
+
+        if (jazziness == Jazziness.LITTLE){
+            chord = getTriad(scale: scale) + getSeventh(scale: scale)
+        }
+
+        if (jazziness == Jazziness.MOAR){
+            chord = getSuspendedVoicing(scale: scale)
+        }
+
+        if (jazziness == Jazziness.JACCOBBB){
+            chord = summonJacob(scale: scale)
+        }
+
+        return addOctaveNotation(chord: chord)
+    }
+
+    enum Jazziness: String, CaseIterable {
+        case NONE = "none"
+        case LITTLE = "little"
+        case MOAR = "moar"
+        case JACCOBBB = "jacob"
     }
 
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        dataSources.count
+        3
     }
 
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        dataSources[component].count
+        switch component {
+        case 0: return rootValues.count
+        case 1: return typeValues.count
+        case 2: return Jazziness.allCases.count
+        default: return 0
+        }
     }
 
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        dataSources[component][row]
+        switch component {
+        case 0: return rootValues[row]
+        case 1: return typeValues[row]
+        case 2: return Jazziness.allCases[row].rawValue
+        default: return nil
+        }
     }
 }
 
