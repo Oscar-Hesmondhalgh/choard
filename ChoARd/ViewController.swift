47aa3cfe7c538776f8a66121f0bce08d3ff69bcf
Cain McCormack <cain.mccormack@focusrite.com>
2022-09-13 14:17:40 +0100

    got a key

--- ChoARd/ViewController.swift
+++ ChoARd/ViewController.swift
@@ -50,19 +50,20 @@ extension ViewController: ARSessionDelegate, ARSCNViewDelegate {
         if let objectAnchor = anchor as? ARObjectAnchor {
 
             print("detected in renderer")
-            let plane = SCNPlane(width: CGFloat(objectAnchor.referenceObject.extent.x * 0.8), height: CGFloat(objectAnchor.referenceObject.extent.y * 0.5))
+            let plane = SCNPlane(width: CGFloat(objectAnchor.referenceObject.extent.x / 2), height: CGFloat(objectAnchor.referenceObject.extent.z / 17))
 
-            plane.cornerRadius = plane.width / 8
+            // plane.cornerRadius = plane.width / 8
 
             let spriteKitScene = SKLabelNode(text: "👾")
 
             plane.firstMaterial?.diffuse.contents = spriteKitScene
             plane.firstMaterial?.isDoubleSided = true
-            plane.firstMaterial?.diffuse.contentsTransform = SCNMatrix4Translate(SCNMatrix4MakeScale(1, -1, 1), 0, 1, 0)
+            // plane.firstMaterial?.diffuse.contentsTransform = SCNMatrix4Translate(SCNMatrix4MakeScale(1, -1, 1), 0, 1, 0)
 
             let planeNode = SCNNode(geometry: plane)
-            // planeNode.transform = SCNMatrix4MakeTranslation(0, 0, 0);
-            planeNode.position = SCNVector3Make(objectAnchor.referenceObject.center.x, objectAnchor.referenceObject.center.y + 0.35, objectAnchor.referenceObject.center.z)
+            planeNode.position = SCNVector3Make(objectAnchor.referenceObject.center.x, objectAnchor.referenceObject.center.y, objectAnchor.referenceObject.center.z)
+            planeNode.rotation = SCNVector4Make(1, 0, 0, .pi * 0.5);
+
 
             node.addChildNode(planeNode)
 
