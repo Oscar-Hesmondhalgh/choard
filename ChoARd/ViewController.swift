0ee7ea8a88cc9050c68ab94512dfde5c31ad0d26
Cain McCormack <cain.mccormack@focusrite.com>
2022-09-13 15:08:58 +0100

    white key names

--- ChoARd/ViewController.swift
+++ ChoARd/ViewController.swift
@@ -1,10 +1,3 @@
-//
-//  ViewController.swift
-//  ChoARd
-//
-//  Created by Cain McCormack on 13/09/2022.
-//
-
 import UIKit
 import RealityKit
 import ARKit
@@ -16,13 +9,8 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Load the "Box" scene from the "Experience" Reality File
-//        boxAnchor = try! Experience.loadBox()
 
         scnView.delegate = self
-
-//        arView.automaticallyConfigureSession = false
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -36,34 +24,26 @@ class ViewController: UIViewController {
         configuration.detectionObjects = ARReferenceObject.referenceObjects(inGroupNamed: "Models", bundle: nil)!
         // configuration.planeDetection = .horizontal
         scnView.session.run(configuration)
-        // arView.scene.anchors.append(boxAnchor!)
-
-
     }
 }
 
 extension ViewController: ARSessionDelegate, ARSCNViewDelegate {
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-
         let node = SCNNode()
 
         if let objectAnchor = anchor as? ARObjectAnchor {
-
             print("detected in renderer")
-
-
             let planeNodes = getKeyPlanes(objectAnchor);
 
             for planeNode in planeNodes {
                 node.addChildNode(planeNode)
             }
-
             // node.addChildNode(planeNodes[0])
-
         }
 
         return node
     }
+
     func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
 //        if anchors[0].name == "Scan_09-44-24" {
 //            guard let object = anchors[0] as? ARObjectAnchor else { return }
@@ -71,11 +51,9 @@ extension ViewController: ARSessionDelegate, ARSCNViewDelegate {
 //        }
     }
 
-    func session(_ session: ARSession, didUpdate anchors: [ARAnchor]) {
-    }
-
     func getKeyPlanes(_ objectAnchor: ARObjectAnchor) -> [SCNNode] {
 
+        let whiteNotes = ["C", "D", "E", "F", "G", "A", "B"]
         var planeNodes = [SCNNode]()
         let keyWidth = objectAnchor.referenceObject.extent.z / 17
         let keyHeight = objectAnchor.referenceObject.extent.x / 2.8
@@ -84,6 +62,7 @@ extension ViewController: ARSessionDelegate, ARSCNViewDelegate {
         let keySize = keyWidth - keyGap
 
         for key in -7...7 {
+            let indexes = (key + 7).quotientAndRemainder(dividingBy: 7)
 
             let plane = SCNPlane(width: CGFloat(keyHeight), height: CGFloat(keySize))
 
@@ -94,9 +73,13 @@ extension ViewController: ARSessionDelegate, ARSCNViewDelegate {
             // plane.firstMaterial?.diffuse.contentsTransform = SCNMatrix4Translate(SCNMatrix4MakeScale(1, -1, 1), 0, 1, 0)
 
             let planeNode = SCNNode(geometry: plane)
+            print(whiteNotes[indexes.remainder] + "\(indexes.quotient)")
+            planeNode.name = whiteNotes[indexes.remainder] + "\(indexes.quotient)"
             planeNode.position = SCNVector3Make(objectAnchor.referenceObject.center.x, objectAnchor.referenceObject.center.y - keyHeightOffset, objectAnchor.referenceObject.center.z + (keyWidth * Float(CGFloat(key))))
             planeNode.rotation = SCNVector4Make(1, 0, 0, .pi * 0.5);
+
             planeNodes.append(planeNode)
+
         }
 
         planeNodes.reverse() //hack
